04977c75fe7144d0ad4d8d83ff93b219b1f2d8bc
sergeiavdeev <sergei_avdeev@mail.ru>
2017-05-15 17:43:47 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,19 +2,16 @@
 require "EasyWay/API/EWConnector.php";
 use EasyWay\API\EWConnector;
 
-//error_reporting(0);
 
 $config = include("config.php");
 
 $ew = new EWConnector($config["url"], $config["user"], $config["pass"]);
 
-//$result = $ew->getTariff("Москва", "Подольск", 12, 0.01);
-//$result = $ew->getPickupPoints();
-
+//Создать заявку
 $order = array(
-    "id" => "3355665585",
-    "locationFrom" => "Москва",
-    "locationTo" => "Подольск",
+    "id" => "3355665586",
+    "locationFrom" => "г Москва, ул Ляпидевского, 18, кв. 1",
+    "locationTo" => "Подольск, Рабочая 32/24",
     "weight" => 10,
     "length" => 50,
     "width" => 60,
@@ -25,10 +22,45 @@ $order = array(
     "paymentMethod" => "1",
     "tariff" => "",
     "deliveryType" => "0",
-    "total" => "300"
+    "total" => "300",
+    "sender" => array(
+        "name" => "ООО \"ОСА индастриз\"",
+        "phones" => array(
+            "phoneNumber" => "905 5089784",
+            "additional" => ""
+        )
+    ),
+    "recipient" => array(
+        "name" => "Иванов И.И.",
+        "phones" => array(
+            "phoneNumber" => "905 5089784",
+            "additional" => ""
+        )
+    ),
+    "items" => array()
 );
 
 $result = $ew->createOrder($order);
 
-header("Content-Type: application/xml; charset=utf-8");
-exit($result);
+if(!$result["isError"]){
+    echo "Заявка создана";    
+}
+else{
+    echo $result["errors"];
+}
+
+//Список ПВЗ
+$result = $ew->getPickupPoints();
+
+echo $result[0]["city"];
+
+//Запрос статусов
+$result = $ew->getStatus(array("27724-YD1846665", "1092105-YD1854018"));
+
+echo $result[0]["status"];
+echo $result[1]["status"];
+
+//Предварительный расчет доставки
+$result = $ew->getTariff("Москва", "Серпухов", 2, 0.1);
+
+echo $result[0]["total"];
