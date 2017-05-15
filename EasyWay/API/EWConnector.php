46cdb883049b600b77aed3507874ca5fc41c090d
sergeiavdeev <sergei_avdeev@mail.ru>
2017-05-15 17:44:48 +0300

    Add files via upload

--- EasyWay/API/EWConnector.php
+++ EasyWay/API/EWConnector.php
@@ -25,7 +25,7 @@ class EWConnector{
     public function getTariff($locationFrom, $locationTo, $weight, $volume){
         
         $url = $this->url."getTariff?locationFrom=".$locationFrom."&locationTo=".$locationTo."&weight=".$weight."&volume=".$volume;
-        return $this->getRequest($url);
+        return json_decode($this->getRequest($url), true);
         
     }
     
@@ -33,10 +33,21 @@ class EWConnector{
     public function getPickupPoints(){
         
         $url = $this->url."getPickupPointsV2";
-        return $this->getRequest($url);
+        return json_decode($this->getRequest($url), true);
     }
     
     public function createOrder($order){
+        
+        $addresFrom = $order["locationFrom"];
+        $addresTo = $order["locationTo"];
+                
+        $order["locationFrom"] = array(
+            "addressString" => $addresFrom
+        );
+        
+        $order["locationTo"] = array(
+            "addressString" => $addresTo
+        );
         
         $params = array(
             "params" => array(
@@ -50,7 +61,32 @@ class EWConnector{
         
         $url = $this->url."createOrder";
         
-        return $this->postRequest($url, $xml->asXML());
+        $result =  $this->postRequest($url, $xml->asXML());
+        return $result;
+    }
+    
+    
+    public function getStatus($orderIds){
+        
+        $query="";
+        
+        for ($i = 0; $i < count($orderIds); $i++){
+            if($i == count($orderIds) - 1){
+                
+                $query = $query.$orderIds[$i];    
+            } else{
+                
+                $query = $query.$orderIds[$i].",";    
+            }
+        }
+        
+        $url = $this->url."getStatus?json=&number=".$query;
+        
+        $result = $this->getRequest($url);
+        
+        //echo $result;
+        
+        return json_decode($result, true);
     }
     
     protected function getRequest($url){
@@ -58,6 +94,8 @@ class EWConnector{
         curl_setopt($this->curl, CURLOPT_URL, $url);
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($this->curl, CURLOPT_HEADER, 0);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array());
+        curl_setopt($this->curl, CURLOPT_POST, false);
         curl_setopt($this->curl, CURLOPT_USERPWD, $this->user.":".$this->pass);
         
         $output = curl_exec($this->curl);
@@ -68,8 +106,7 @@ class EWConnector{
     protected function postRequest($url, $data){
         
         curl_setopt($this->curl, CURLOPT_URL, $url);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($this->curl, CURLOPT_HEADER, 0);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);        
         curl_setopt($this->curl, CURLOPT_POST, true);
         curl_setopt($this->curl, CURLOPT_USERPWD, $this->user.":".$this->pass);
         
@@ -81,7 +118,13 @@ class EWConnector{
         
         $output = curl_exec($this->curl);
         
-        return $output;
+        $arr = $this->to_array($output);
+        
+        return array(
+            "isError" => $arr["fault"]["isError"],
+            "errors" => $arr["fault"]["errors"],
+            "id" => $arr["value"]
+        );
     }
     
     
@@ -95,5 +138,15 @@ class EWConnector{
                 $object->addChild($key, $value);
             }
         }
+    }
+    
+    
+    private function to_array ($string)
+    {
+        $xml   = simplexml_load_string($string, 'SimpleXMLElement', LIBXML_NOCDATA);
+        
+        $array = json_decode(json_encode($xml, JSON_UNESCAPED_SLASHES), TRUE);
+        
+        return $array;
     }
 }
